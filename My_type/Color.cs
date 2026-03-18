4dc377e5dbbc793244dbde831f8f4a37b70b84d0
Cat-Communist <popovvp2502@gmail.com>
2026-03-18 12:46:29 +0800

    started RGB class

---
+++ My_type/Color.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyType
+{
+    public class RGB
+    {
+        private int red;
+        private int green;
+        private int blue;
+
+        public RGB(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+    }
+}
